fe1852e3afc02e1f6eae61c921c46d26e075c46c
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-09-02 18:26:17 +0200

    remove todo function

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,17 @@ function App() {
     })
   }
 
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((item) => item.id !== todoId)
+    })
+  }
+
   return (
     <>
     <div>
       <NewTodo onAddToDo={addToDoHandler}/>
-      <Todos items={todos}/>
+      <Todos items={todos} onRemoveTodo={removeTodoHandler}/>
     </div>
     </>
   )
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -5,10 +5,10 @@ import classes from "./Todos.module.css"
 //React.FC è il type per i functional component
 //scrivendo la sintassi dei generics a React.FC ho la possibilità di unire le mie custom props come items con la prop speciale children,
 //che altrimenti avrei problemi a definire
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (props) => {
     return (
         <ul className={classes.todos}>
-            {props.items.map((item) => (<TodoItem key={item.id} todoText={item.text} />))}
+            {props.items.map((item) => (<TodoItem key={item.id} todoText={item.text} onRemoveTodo={props.onRemoveTodo.bind(null, item.id)} />))}
         </ul>
     )
 }
